19b64069a878cb216d1e554c99be4b19c96bba5f
Sveta <serikova-ss@mail.ru>
2022-11-29 22:53:07 +0300

    add regression results with comprehensive table with statistical info

--- folders/prediction/linear_regression.py
+++ folders/prediction/linear_regression.py
@@ -1,9 +1,12 @@
 import numpy as np
+import pandas as pd
+from scipy import stats
 import streamlit as st
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
+import statsmodels.api as sm
 
 def start_linear(dataset_drop_outcome, outcome, x_train,y_train,x_test,y_test,user_input_variables):
 
@@ -17,13 +20,16 @@ def start_linear(dataset_drop_outcome, outcome, x_train,y_train,x_test,y_test,us
     st.write(lin_model.intercept_)
 
     # k независимых переменных
-    st.subheader('Коэффициент при x: ')
-    st.write(lin_model.coef_)
+    # st.subheader('Коэффициент при x: ')
+    # st.write(lin_model.coef_)
 
-    # точность классификации
-    st.subheader('Средняя абсолютная ошибка: ')
-    st.write(mean_absolute_error(y_test, lin_model.predict(x_test)))
-
+    st.subheader('Коэффициент при x и статистическая значимость коэффициентов (p-value): ')
+    x = sm.add_constant(x_train) # adding a constant
+    model = sm.OLS(y_train, x_train).fit()
+    predictions = model.predict(x_test) 
+    print_model = model.summary()
+    d = {'coef': model.params, 'p-values': [float(np_float) for np_float in model.pvalues], 't-value': model.tvalues, 'std_error': model.bse}
+    st.write(pd.DataFrame(data=d))
 
     prediction_tree=lin_model.predict(user_input_variables)
     st.subheader('Предсказание:')
@@ -33,3 +39,7 @@ def start_linear(dataset_drop_outcome, outcome, x_train,y_train,x_test,y_test,us
             st.write("Negative")
         else:
             st.write("Positive")
+
+    # точность классификации
+    st.subheader('Средняя абсолютная ошибка: ')
+    st.write(mean_absolute_error(y_test, lin_model.predict(x_test)))
